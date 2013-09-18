a1ec3ef462ee534d1e63244ebcae70161fe356ee
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2013-09-18 02:34:26 -0400

    Fixed sample data csv.

---
+++ Challenge 1/SendTempData/GUI/_site/src/tests/org/sunspotworld/demo/SendDataDemoGuiHostApplicationTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.org.sunspotworld.demo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.sunspotworld.demo.SendDataDemoGuiHostApplication;
+
+/**
+ *
+ * @author Aaron Heuckroth
+ */
+public class SendDataDemoGuiHostApplicationTest {
+    
+    public SendDataDemoGuiHostApplicationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class SendDataDemoGuiHostApplication.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        SendDataDemoGuiHostApplication.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
