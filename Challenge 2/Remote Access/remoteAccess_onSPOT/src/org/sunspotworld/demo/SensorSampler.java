6a0958f8f00c3defee4342b7c897ffb8cf22c023
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2013-09-26 14:58:05 -0400

    Added new project files for Challenge 3.

---
+++ Challenge 2/Remote Access/remoteAccess_onSPOT/src/org/sunspotworld/demo/SensorSampler.java
@@ -0,0 +1,125 @@
+/*
+ * SensorSampler.java
+ *
+ * Copyright (c) 2008-2010 Sun Microsystems, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package org.sunspotworld.demo;
+
+import com.sun.spot.io.j2me.radiogram.*;
+import com.sun.spot.resources.Resources;
+import com.sun.spot.resources.transducers.ITriColorLED;
+import com.sun.spot.resources.transducers.ITemperatureInput;
+import com.sun.spot.util.Utils;
+import javax.microedition.io.*;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * This application is the 'on SPOT' portion of the SendDataDemo. It
+ * periodically samples a sensor value on the SPOT and transmits it to
+ * a desktop application (the 'on Desktop' portion of the SendDataDemo)
+ * where the values are displayed.
+ *
+ * @author: Vipul Gupta
+ * modified: Ron Goldman
+ */
+public class SensorSampler extends MIDlet {
+
+    private static final int HOST_PORT = 99;
+    private static final int SAMPLE_PERIOD = 3000;  // in milliseconds
+    
+    protected void startApp() throws MIDletStateChangeException {
+        RadiogramConnection rCon = null;
+        Datagram dg = null;
+        //String ourAddress = System.getProperty("IEEE_ADDRESS");
+        //ITemperatureInput tempSensor = (ITemperatureInput) Resources.lookup(ITemperatureInput.class);
+        ITriColorLED led = (ITriColorLED)Resources.lookup(ITriColorLED.class, "LED7");
+        
+        //System.out.println("Starting sensor sampler application on " + ourAddress + " ...");
+
+	// Listen for downloads/commands over USB connection
+	new com.sun.spot.service.BootloaderListenerService().getInstance().start();
+
+        try {
+            // Open up a broadcast connection to the host port
+            // where the 'on Desktop' portion of this demo is listening
+            rCon = (RadiogramConnection) Connector.open("radiogram://:" + HOST_PORT);
+            dg = (Radiogram) rCon.newDatagram(rCon.getMaximumLength());
+        } catch (Exception e) {
+            System.err.println("Caught " + e + " in connection initialization.");
+            notifyDestroyed();
+        }
+        
+        System.out.println("Listening on port " + HOST_PORT + "...\n");
+        
+        while (true) {
+            try {
+                // Get the current time and sensor reading
+                //long now = System.currentTimeMillis();
+                //double reading = tempSensor.getCelsius();
+                
+                
+                
+                rCon.receive(dg);
+                String message = dg.readUTF();
+                System.out.println("Message received: " + message);
+                led.setRGB(255,255,255);
+                
+                if (message.equals("on")){
+                    led.setOn();
+                    System.out.println("LED turned on.");
+                }
+                else if (message.equals("off")){
+                    led.setOff();
+                    System.out.println("LED turned off.");
+                }
+               
+                // Flash an LED to indicate a sampling event
+                //led.setRGB(255, 255, 255);
+                //led.setOn();
+                //Utils.sleep(50);
+                //led.setOff();
+
+                // Package the time and sensor reading into a radio datagram and send it.
+                //dg.reset();
+                //dg.writeLong(now);
+                //dg.writeDouble(reading);
+                //rCon.send(dg);
+
+                //System.out.println("Temperature value = " + reading);
+                
+                // Go to sleep to conserve battery
+                //Utils.sleep(SAMPLE_PERIOD - (System.currentTimeMillis() - now));
+            } catch (Exception e) {
+                System.err.println("Caught " + e + " while collecting/sending sensor sample.");
+            }
+        }
+    }
+    
+    protected void pauseApp() {
+        // This will never be called by the Squawk VM
+    }
+    
+    protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+        // Only called if startApp throws any exception other than MIDletStateChangeException
+    }
+}
