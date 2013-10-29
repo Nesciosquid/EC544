a5f7485944fe15c4fcc3e6750e285b63894b9816
Erik-k <enk@signals27.ad.bu.edu>
2013-10-29 15:48:55 -0400

    Builds and loads

--- URange/src/org/sunspotworld/URange.java
+++ URange/src/org/sunspotworld/URange.java
@@ -2,14 +2,15 @@
  * URange.java
  *
  * Created on Jul 4, 2012 4:35:45 PM;
+ * Modified Oct 29, 2013
  */
 
 package org.sunspotworld;
 
 import com.sun.spot.peripheral.radio.RadioFactory;
 import com.sun.spot.resources.Resources;
-import com.sun.spot.resources.transducers.IIOPin;
-import com.sun.spot.resources.transducers.IAnalogInput;
+import com.sun.spot.resources.transducers.IInputPin;
+//import com.sun.spot.resources.transducers.IAnalogInput;
 //import com.sun.spot.sensorboard.io.AnalogInput;
 import com.sun.spot.sensorboard.EDemoBoard;
 import com.sun.spot.sensorboard.io.PinDescriptor;
@@ -43,91 +44,54 @@ import javax.microedition.io.StreamConnection;
  * @author  Erik Knechtel
  */
 
-// Serial code examples from:
-// http://www.csc.kth.se/utbildning/kth/kurser/DH2400/interak08/6-Comm.html
-// http://www.csc.kth.se/utbildning/kth/kurser/DH2400/interak08/6-SpotComm.java
+/* Note that the ultrasonic range finder uses specular reflections,
+* which require that the object it is locating have some part of its surface
+* oriented parallel with the sensor. So testing this with a flat notebook, 
+* if the notebook is tilted then the range finder cannot see it and detects 
+* infinite distance. With a person or other round object, there will always
+* be some reflection back to the sensor so this problem is avoided.
+*/
 
 public class URange extends MIDlet {
 
     private IInputPin rxPin = EDemoBoard.getInstance().getIOPins()[EDemoBoard.D0];
     private IInputPin txPin = EDemoBoard.getInstance().getIOPins()[EDemoBoard.D1];
+    private EDemoBoard demoBoard = EDemoBoard.getInstance();
     
     protected void startApp() throws MIDletStateChangeException {
         System.out.println("Ultrasonic MaxSonar in action...");
         BootloaderListenerService.getInstance().start();   // monitor the USB (if connected) and recognize commands from host
-    
-    private void byteUartCommunication(){
-        EDemoBoard.getInstance().initUART(9600, false);
+        demoBoard.initUART(9600, false);
         while(true){
+            System.out.println("In while loop");
+            String returnString = "";
+            byte[] buffer = new byte[64];
+            
             try{
-                System.out.print((char)EDemoBoard.getInstance().receiveUART());
-            }catch(IOException e){
+                if (demoBoard.availableUART()>1){
+                demoBoard.readUART(buffer, 0, buffer.length);
+                returnString = returnString + new String(buffer, "US-ASCII").trim();
+                System.out.println(returnString);
+                System.out.println("Finished getting string");
+                }
+            } catch(IOException e) {
                 if(e.getMessage().equals("empty uart buffer"))
                     Utils.sleep(100);
                 else
                     System.out.println(e);
             }
-        }
-    }
-    
-    // "this is untested, should work with the Purple API" according to the source I cribbed this from
-    private void streamUartCommunication() throws IOException {
-        InputStream is= Connector.openInputStream("edemoserial://usart?baudrate=9600");
-        byte[]buffer= new byte[256];
-        int length=-1;
-        while((length=is.read(buffer))!=-1){
-            System.out.write(buffer, 0, length);
-        }
-    }
-    
-    private void run()throws IOException {
-        streamUartCommunication();
-        byteUartCommunication();
-    }
-        //IAnalogInput sensor = EDemoBoard.getInstance().getAnalogInputs()[EDemoBoard.A0];
-        
-   while(true){
-        try {
-                /* Note that the ultrasonic range finder uses specular reflections,
-             * which require that the object it is locating have some part of its surface
-             * oriented parallel with the sensor. So testing this with a flat notebook, 
-             * if the notebook is tilted then the range finder cannot see it and detects 
-             * infinite distance. With a person or other round object, there will always
-             * be some reflection back to the sensor so this problem is avoided.
-             * 
-             */
-            
-            // Analog usage:
-//                double vcc = 3.3; // Change this depending on power supply
-//                double v_measured = sensor.getVoltage();
-//                double v_perInch = vcc/512;
-//                double range = v_measured/v_perInch;
-//                System.out.println("Range: "+ range);  
-//           //     led.setOff();
-//                Utils.sleep(1000);
-            // End of Analog usage code.
-            
-            // Serial usage: tie BW pin low
-            run();
-            // End of Serial usage code.
-            } catch (IOException ex){
-                ex.printStackTrace();
+            try{
+                Thread.sleep(1000);
             }
-          //  Utils.sleep(100);
-     //       notifyDestroyed();
+            catch (InterruptedException ie){
+            
+            }
         }
-     }
-
+    }
+  
     protected void pauseApp() {
-        // This is not currently called by the Squawk VM
     }
 
-    /**
-     * Called if the MIDlet is terminated by the system.
-     * It is not called if MIDlet.notifyDestroyed() was called.
-     *
-     * @param unconditional If true the MIDlet must cleanup and release all resources.
-     */
     protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
     }
 }
